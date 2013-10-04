6a54d1d1261d74486bcc2f3ea52664ae37551bf5
Park Hyunwoo <ez@smartstudy.co.kr>
2013-10-04 21:55:06 +0900

    Update to v1.9.1

--- npk.rb
+++ npk.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Npk < Formula
   homepage 'http://github.com/lqez/npk/'
   url 'https://github.com/lqez/npk/archive/v1.9.tar.gz'
-  sha1 'c15069e9aeada2b09d209f5d3ce492e6668e53fb'
+  sha1 'a76bea612a7e2e30bb57636e8b52a6e45a991214'
 
   depends_on 'cmake' => :build
 
