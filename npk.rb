99e21837b36bdbcaf8ebfb0beb1ef20ea5c39639
Park Hyunwoo <ez@smartstudy.co.kr>
2013-10-04 22:07:24 +0900

    Fix URL

--- npk.rb
+++ npk.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Npk < Formula
   homepage 'http://github.com/lqez/npk/'
-  url 'https://github.com/lqez/npk/archive/v1.9.tar.gz'
+  url 'https://github.com/lqez/npk/archive/v1.9.1.tar.gz'
   sha1 'a76bea612a7e2e30bb57636e8b52a6e45a991214'
 
   depends_on 'cmake' => :build
